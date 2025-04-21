780dd81099090230fc616083ad87166e32909fb0
IG0R31 <igor.oliveiraduarte05@gmail.com>
2025-04-21 10:35:16 -0300

    começando do zero

---
+++ AVL.h
@@ -0,0 +1,30 @@
+#ifndef AVL_H
+#define AVL_H
+
+typedef int TIPOCHAVE;
+typedef struct aux {
+	TIPOCHAVE chave;
+	struct aux *esq;
+	struct aux *dir;
+	int bal; 
+} NO, *PONT;
+
+PONT criarNovoNo(TIPOCHAVE ch);
+int max(int a, int b);
+int altura(PONT p);
+void exibirArvoreEmOrdem(PONT raiz);
+void exibirArvorePreOrdem(PONT raiz);
+void exibirArvorePosOrdem(PONT raiz);
+void exibirArvore(PONT raiz);
+PONT rotacaoL(PONT p);
+PONT rotacaoR(PONT p);
+void inserirAVL(PONT* pp, TIPOCHAVE ch, bool* alterou);
+PONT buscaBinaria(TIPOCHAVE ch, PONT raiz);
+PONT buscaNo(PONT raiz, TIPOCHAVE ch, PONT *pai);
+PONT maiorAEsquerda(PONT p, PONT *ant);
+bool excluirAVL(PONT* raiz, TIPOCHAVE ch, bool* alterou);
+void destruirAux(PONT subRaiz);
+void destruirArvore(PONT * raiz);
+void inicializar(PONT * raiz);
+
+#endif // AVL_H
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h> 
+#include "AVL.h"
+
+int main(){
+    
+}
